af7c1ec06317e1c57041fc54efec408f36421db1
吴翀 <wuchyuz@gmail.com>
2019-07-23 18:21:44 +0800

    init

---
+++ packages/index.js
@@ -0,0 +1,5 @@
+import Viewer from './viewer'
+
+Viewer.__version__ = '1.0.0' // webapack
+
+export default Viewer
---
+++ packages/template.js
@@ -0,0 +1,7 @@
+export default (
+  `
+    <div class="viewer-container" touch-action="none">
+      <div class="viewer-wrap"></div>
+    </div>
+  `
+)
