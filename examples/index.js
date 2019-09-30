7b64ffdae5ce96e78f291f1c293ba378398ceea0
吴翀 <wuchyuz@gmail.com>
2019-09-30 17:42:14 +0800

    refactor: hammer test

--- examples/index.js
+++ examples/index.js
@@ -1,8 +1,18 @@
 import Viewer from '../src'
 import '../src/style/index.css'
+import Hammer from 'hammerjs'
 
 let source = document.getElementById('container')
 let v = new Viewer(source)
 source.addEventListener('click', function () {
   v.show()
 })
+
+let h = document.getElementById('h')
+var hammer = new Hammer(h)
+console.log(hammer)
+hammer.on('tapstart', function(e) {
+  // e.target.classList.toggle('expand');
+  console.log("You're pressing me!");
+  console.log(e);
+});
